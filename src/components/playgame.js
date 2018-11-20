cd6d2dbeeffa64c724d07081ff4ad8356634f483
Daniel Shimon <danielzshimon@gmail.com>
2018-11-20 12:17:00 -0500

    made first page a bit better

--- src/components/playgame.js
+++ src/components/playgame.js
@@ -2,16 +2,28 @@ import React from "react";
 import { connect } from "react-redux";
 import { startGame } from '../actions/index'
 import { Button } from 'semantic-ui-react'
+import grass_2 from '../images/grass_2.png'
 
 const StartGame = (props) => {
     
     return (
-        <div align='center'>
-            <h1>Endless Kickoff Return</h1>
-            <h2> USE THE ARROW KEYS TO MOVE </h2>
-            <h2>LOSE WHEN YOU HIT AN OPPOSING PLAYER</h2>
-            <Button inverted color='green' onClick={props.startGame}> Start Game </Button>   
-            
+        <div  style= {{backgroundImage: `url(${grass_2})`,
+        height: '1000px',
+        textAlign:'center'
+         }}>
+            <div align='center' position='center' style={{
+            position: 'center',
+            width: '420px',
+            border: '2px solid pink',
+            backgroundColor: 'white',
+            display: 'inline-block',
+            margin: '100px'
+            }}>
+                <h2>Endless Kickoff Return</h2>
+                <h3> USE THE ARROW KEYS TO MOVE </h3>
+                <h3>LOSE WHEN YOU HIT AN OPPOSING PLAYER</h3>
+                <Button inverted color='green' onClick={props.startGame}> Start Game </Button>   
+            </div>
        </div>
     );
   };
